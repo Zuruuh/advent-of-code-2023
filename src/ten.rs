26d7955acedd63f857c4efaf8bf2eb273350e034
Zuruuh <ziadi.mail.pro@gmail.com>
2023-12-10 16:48:43 +0100

    refacto day 10 and finish part 1

--- src/ten.rs
+++ src/ten.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, fmt};
 
 use crate::Position;
 
@@ -11,11 +11,6 @@ enum Pipe {
     BottomRight,
     TopLeft,
     TopRight,
-}
-
-enum PipeSurroundingPositionResult {
-    PotentialPositions(Vec<Position>),
-    NotComputable,
 }
 
 impl Pipe {
@@ -32,21 +27,94 @@ impl Pipe {
         })
     }
 
-    pub fn surrounding(&self, position: &Position) -> PipeSurroundingPositionResult {
-        PipeSurroundingPositionResult::PotentialPositions(match self {
-            Pipe::Start => return PipeSurroundingPositionResult::NotComputable,
+    pub fn surrounding(&self, position: &Position) -> Vec<Position> {
+        match self {
             Pipe::Vertical => vec![position.next_x(), position.prev_x()],
             Pipe::Horizontal => vec![position.next_y(), position.prev_y()],
             Pipe::BottomLeft => vec![position.prev_x(), position.next_y()],
             Pipe::BottomRight => vec![position.prev_x(), position.prev_y()],
             Pipe::TopLeft => vec![position.next_x(), position.next_y()],
             Pipe::TopRight => vec![position.next_x(), position.prev_y()],
-        })
+            _ => panic!(""),
+        }
+    }
+}
+
+#[derive(Clone)]
+struct PipeIterator {
+    grid: BTreeMap<Position, Option<Pipe>>,
+    starting_position: Position,
+    current_position: Position,
+    previous_position: Position,
+}
+
+impl PipeIterator {
+    pub fn new(grid: BTreeMap<Position, Option<Pipe>>) -> Self {
+        let starting_position = **grid
+            .iter()
+            .filter_map(|(pos, pipe)| matches!(pipe, Some(Pipe::Start)).then(|| pos))
+            .collect::<Vec<_>>()
+            .first()
+            .expect("Could not find starting square");
+
+        Self {
+            grid,
+            starting_position,
+            current_position: starting_position,
+            previous_position: starting_position,
+        }
+    }
+}
+
+impl FromIterator<(Position, Option<Pipe>)> for PipeIterator {
+    fn from_iter<T: IntoIterator<Item = (Position, Option<Pipe>)>>(iter: T) -> Self {
+        Self::new(iter.into_iter().collect::<BTreeMap<_, _>>())
+    }
+}
+
+impl Iterator for PipeIterator {
+    type Item = Position;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.starting_position == self.current_position {
+            self.current_position = self
+                .starting_position
+                .surrounding_without_diagonals()
+                .iter()
+                .find_map(|pos| match self.grid.get(&pos) {
+                    None => None,
+                    Some(_) => Some(*pos),
+                })
+                .expect("Could not find a valid pipe next to starting position");
+
+            return Some(self.current_position);
+        }
+
+        let current_pipe = self
+            .grid
+            .get(&self.current_position)
+            .expect("Position is not contained in grid")
+            .expect("Should not be currently on an empty cell");
+
+        let next_position = current_pipe
+            .surrounding(&self.current_position)
+            .into_iter()
+            .find(|pos| pos != &self.previous_position && pos != &self.starting_position);
+
+        match next_position {
+            Some(next_position) => {
+                self.previous_position = self.current_position;
+                self.current_position = next_position;
+
+                Some(next_position)
+            }
+            None => None,
+        }
     }
 }
 
 pub fn ten(input: &str) -> usize {
-    let grid = input
+    (input
         .lines()
         .enumerate()
         .flat_map(|(x, line)| {
@@ -55,51 +123,11 @@ pub fn ten(input: &str) -> usize {
                 .map(|(y, char)| (Position { x, y }, Pipe::from_char(char)))
                 .collect::<Vec<_>>()
         })
-        .collect::<BTreeMap<_, _>>();
-
-    let starting_position = **grid
-        .iter()
-        .filter_map(|(pos, pipe)| matches!(pipe, Some(Pipe::Start)).then(|| pos))
-        .collect::<Vec<_>>()
-        .first()
-        .expect("Could not find starting square");
-
-    let mut distance_grid = BTreeMap::<Position, usize>::new();
-    // distance_grid.insert(*&starting_position, 0);
-    explore(
-        (&starting_position, &Pipe::Start),
-        &grid,
-        &mut distance_grid,
-        0,
-    );
-
-    1
-}
-
-fn explore(
-    cell: (&Position, &Pipe),
-    grid: &BTreeMap<Position, Option<Pipe>>,
-    distance_grid: &mut BTreeMap<Position, usize>,
-    index: usize,
-) {
-    // for surrounding_position in cell.0.surrounding() {
-    // match grid.get(&surrounding_position) {
-    //     None => continue,
-    //     Some(maybe_surrounding_pipe) => match maybe_surrounding_pipe {
-    //         None => continue,
-    //         Some(surrounding_pipe) => match surrounding_pipe {
-    //             Pipe::Start => { /*TODO: Check if current cell can go to start*/ }
-    //             Pipe::Vertical => {}
-    //             _ => {}
-    //         },
-    //     },
-    // }
-    // }
-
-    match cell.1.surrounding(cell.0) {
-        PipeSurroundingPositionResult::NotComputable => todo!(),
-        PipeSurroundingPositionResult::PotentialPositions(positions) => todo!(),
-    }
+        .collect::<PipeIterator>()
+        .into_iter()
+        .count()
+        / 2)
+        + 1
 }
 
 #[cfg(test)]
@@ -111,6 +139,6 @@ mod test {
         let input = include_str!("./ten.txt");
         let output = ten(input);
 
-        assert_eq!(1, output);
+        assert_eq!(4, output);
     }
 }
